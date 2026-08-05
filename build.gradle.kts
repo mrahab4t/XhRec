38d562bc78aaa7956bed16bcf6b4e5106c72d70f
scott olsen <scott@onetworking.net>
2026-08-05 11:52:46 -0500

    bump org.jline:jline from 3.30.6 to 4.0.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
 
     implementation("io.github.nomisrev:kotlinx-serialization-jsonpath:1.0.0")
     implementation("org.jsoup:jsoup:1.22.1")
-    implementation("org.jline:jline:3.30.6")
+    implementation("org.jline:jline:4.0.9")
     implementation("commons-cli:commons-cli:1.11.0")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
